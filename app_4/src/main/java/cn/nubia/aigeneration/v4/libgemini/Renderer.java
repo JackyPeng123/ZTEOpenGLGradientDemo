16fd474402d104fe9871c81adec25375aafd6a66
pengchengkun <873141139@qq.com>
2026-06-28 17:25:12 +0800

    提交【辅助构图】基础结构

---
+++ app_4/src/main/java/cn/nubia/aigeneration/v4/libgemini/Renderer.java
@@ -0,0 +1,36 @@
+package cn.nubia.aigeneration.v4.libgemini;
+
+import android.graphics.Bitmap;
+
+public interface Renderer {
+    /**
+     * 恢复播放
+     */
+    void resume();
+
+    /**
+     * 暂停播放
+     */
+    void pause();
+
+    /**
+     * 设置四边圆角
+     * @param radius 为0表示无圆角
+     */
+    void setRoundCorner(float radius);
+
+    /**
+     * 【生成中】的图片
+     */
+    void setGeneratingBitmap(Bitmap bitmap);
+
+    /**
+     * 【已生成】的图片位于【生成中】图片的左上右下间距
+     */
+    void setGeneratedPixelOffset(float left, float top, float right, float bottom);
+
+    /**
+     * 变更状态
+     */
+    void performChangeState(@GradientRenderer.State int state);
+}
